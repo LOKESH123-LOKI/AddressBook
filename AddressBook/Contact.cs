21a20687310310a4e16e573ded43bd7deda40f8a
mahesh <lokilokesh1680@gmail.com>
2023-03-14 20:00:12 +0530

    [lokesh]add:folder is added

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Contact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string state { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace AddressBook
+{
+    public class program
+    {
+        public void Main(string[] args)
+        {
+            Contact contact = new Contact();
+            Console.WriteLine("Enter FirstName");
+            contact.firstName = Console.ReadLine();
+            Console.WriteLine("Enter lastName");
+            contact.lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            contact.state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            contact.Zip = Console.ReadLine();
+            Console.WriteLine("Enter PhoneNumber");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email");
+            contact.Email = Console.ReadLine();
+        }
+    }
+}
